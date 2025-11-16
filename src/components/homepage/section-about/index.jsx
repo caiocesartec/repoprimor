e315bd0ea91d4ae4f2a4e0a2a4c654d8775b400f
Caio Alves <newcesar25@gmail.com>
2025-11-16 15:15:00 -0300

    ajuste de imagem

--- src/components/homepage/section-about/index.jsx
+++ src/components/homepage/section-about/index.jsx
@@ -14,6 +14,7 @@ export const SectionAbout = () => {
             width={600}
             height={400}
             className={styles.image}
+            quality={100}
           />
         </div>
 
