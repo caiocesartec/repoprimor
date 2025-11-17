3552cc89979d695ab4ebabe3b1bea4445e13192d
Caio Alves <newcesar25@gmail.com>
2025-11-17 01:04:14 -0300

    Cadastro de Receitas

--- src/lib/filiais.js
+++ src/lib/filiais.js
@@ -3,34 +3,53 @@ export async function getReceitas() {
 
   return [
     {
-      nome: "Nome receita 1",
+      nome: "Pastel Assado com Carne-seca e Requeijão",
       endereco: "Rua Exemplo, 123 - Centro, São Paulo/SP",
-      imagem: { url: "/receitas/bolo-chocolate.jpg" },
+      imagem: { url: "/receitas/pastel.jpg" },
+      linkReceita: {url: "https://www.receiteria.com.br/pastel-assado-com-carne-seca-e-requeijao/"},
     },
     {
-      nome: "Nome receita 2",
+      nome: "Macarrão Gratinado com Cheddar e Bacon",
       endereco: "Av. Placeholder, 456 - Curitiba/PR",
-      imagem: { url: "/receitas/acompanhamentos.png" },
+      imagem: { url: "/receitas/macarrao.jpg" },
+      linkReceita: {url: "https://www.delicia.com.br/receita/macarrao-gratinado-com-cheddar-e-bacon/"},
+
     },
     {
-      nome: "Nome receita 3",
+      nome: "Enformado de Batata com Lascas de Bacalhau à Portuguesa",
       endereco: "Praça Mock 789 - Belo Horizonte/MG",
-      imagem: { url: "/receitas/panqueca-frango.jpg" },
+      imagem: { url: "/receitas/baca.jpg" },
+      linkReceita: {url: "https://www.delicia.com.br/receita/enformado-de-batata-com-lascas-de-bacalhau-a-portuguesa/"},
     },
     {
-      nome: "Nome receita 4",
-      endereco: "Rua Exemplo, 123 - Centro, São Paulo/SP",
-      imagem: { url: "/receitas/bolo-chocolate.jpg" },
-    },
-    {
-      nome: "Nome receita 5",
-      endereco: "Av. Placeholder, 456 - Curitiba/PR",
-      imagem: { url: "/receitas/acompanhamentos.png" },
-    },
-    {
-      nome: "Nome receita 6",
+      nome: "Fiesta Seara Assado com Farofa de Legumes",
       endereco: "Praça Mock 789 - Belo Horizonte/MG",
-      imagem: { url: "/receitas/panqueca-frango.jpg" },
+      imagem: { url: "/receitas/frango.jpg" },
+      linkReceita: {url: "https://www.delicia.com.br/receita/fiesta-seara-assado-com-farofa-de-legumes/"},
+    },
+    {
+      nome: "Crespinhos de Batata",
+      endereco: "Praça Mock 789 - Belo Horizonte/MG",
+      imagem: { url: "/receitas/crespinho.jpg" },
+      linkReceita: {url: "https://www.delicia.com.br/receita/crespinhos-de-batata/"},
+    },
+    {
+      nome: "Bolo de Chocolate Delícia",
+      endereco: "Praça Mock 789 - Belo Horizonte/MG",
+      imagem: { url: "/receitas/bolo.jpg" },
+      linkReceita: {url: "https://www.delicia.com.br/receita/bolo-de-chocolate-delicia/"},
+    },
+    {
+      nome: "Hamburguinho com Molho de Alho",
+      endereco: "Praça Mock 789 - Belo Horizonte/MG",
+      imagem: { url: "/receitas/hamb.jpg" },
+      linkReceita: {url: "https://www.delicia.com.br/receita/hamburguinho-com-molho-de-alho/"},
+    },
+    {
+      nome: "Brownie de chocolate",
+      endereco: "Praça Mock 789 - Belo Horizonte/MG",
+      imagem: { url: "/receitas/brow.jpg" },
+      linkReceita: {url: "https://www.delicia.com.br/receita/brownie-de-chocolate/"},
     },
   ];
 }
