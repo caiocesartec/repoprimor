387d09fc2dcfec7004b2eda3af7958aa9b49c47d
Guilherme Pappi <guilhermepappi@MacBook-Air-de-Guilherme.local>
2025-11-17 07:17:05 -0300

    adiciona banner mobile

--- src/components/homepage/section-banner/index.jsx
+++ src/components/homepage/section-banner/index.jsx
@@ -5,28 +5,28 @@ import { BannerClient } from "./banner-client";
 const branches = [
   {
     image: "/banners/banner_principal.jpg",
-    imageMobile: "/banners/banner_principal.jpg",
+    imageMobile: "/banners/mobile/principal.png",
     title: "A cremosa que vira o jogo",
     text: "Delícia com creme de leite.",
     textTwo: "Pra deixar qualquer momento ainda mais gostoso.",
   },
   {
     image: "/banners/Banner_Cozinhando.jpg",
-    imageMobile: "/banners/Banner_Cozinhando.jpg",
+    imageMobile: "/banners/mobile/cozinhando.png",
     title: "O sabor que surpreende",
     text: "A única cremosa de verdade.",
     textTwo: "Do fogão ao prato, tudo vira UOU!",
   },
   {
     image: "/banners/banner_principal.jpg",
-    imageMobile: "/banners/banner_principal.jpg",
+    imageMobile: "/banners/mobile/principal.png",
     title: "Vira sabor, vira Delícia",
     text: "Cremosidade que faz a diferença.",
     textTwo: "Porque nada tá tão bom que não possa ficar ainda melhor.",
   },
   {
     image: "/banners/Banner_Cozinhando.jpg",
-    imageMobile: "/banners/Banner_Cozinhando.jpg",
+    imageMobile: "/banners/mobile/cozinhando.png",
     title: "Mais sabor, mais cremosidade",
     text: "Pra cozinhar, espalhar e surpreender.",
     textTwo: "Transforme o simples em deliciosamente incrível.",
