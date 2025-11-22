d9f9d2145817e7b7d853828986bad7f920b5f5c6
Guilherme Pappi <guilhermepappi@MacBook-Air-de-Guilherme.local>
2025-11-22 15:35:40 -0300

    retorna secao de Nossos produtos

--- src/components/homepage/section-three-product-show/index.jsx
+++ src/components/homepage/section-three-product-show/index.jsx
@@ -2,13 +2,24 @@ import { getProdutos } from "@/lib/produtos";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./styles.module.css";
+import { Wave } from "@/components/common/waves";
 
 export const SectionThreeProductShow = async () => {
   const produtos = await getProdutos();
 
   return (
     <section className={styles.SectionThreeProductShow}>
+      <Wave direction="up" />
+
       <div className={styles.containerFull}>
+      <div className={styles.productContainer}>
+          <h2 className={styles.productTitleCall}>Nossos Produtos</h2>
+
+          <p className={styles.descriptionProducts}>
+            A receita garante mais sabor e cremosidade para as suas refeições.
+            Em opções com sal e sem sal, disponíveis em todo o país.
+          </p>
+        </div>
         
         <div className={styles.productsGrid}>
           {produtos.map((p) => (
