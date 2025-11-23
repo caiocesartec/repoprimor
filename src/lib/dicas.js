13267bf7e44d21523b8d25b9505f07dc38cd530e
Guilherme Pappi <guilhermepappi@MacBook-Air-de-Guilherme.local>
2025-11-23 16:14:47 -0300

    try/catch to API fetch

--- src/lib/dicas.js
+++ src/lib/dicas.js
@@ -1,4 +1,3 @@
-
 export async function getDicas() {
   const query = `
     {
@@ -18,27 +17,34 @@ export async function getDicas() {
         }
       }
     }
-  `;  
+  `;
 
   function mapDica(dica) {
     const acf = dica?.camposDaDica ?? {};
-  
+
     return {
       titulo: acf?.nomeDaDica ?? "",
       detalhesDaDica: acf?.descricaoDaDica ?? "",
       imagem: acf?.imagem?.node?.mediaItemUrl || "/placeholder.png",
       href: acf?.linkDaDica || null,
     };
-  } 
+  }
 
-  const response = await fetch("https://new.primor.com.br/graphql", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ query }),
-    cache: "no-store",
-  });
+  let data;
+  try {
+    const response = await fetch("https://new.primor.com.br/graphql", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query }),
+      cache: "no-store",
+    });
 
-  const { data } = await response.json();
+    const json = await response.json();
+    data = json.data;
+  } catch (err) {
+    // console.error(err);
+    return [];
+  }
 
   return data?.dicas?.nodes?.map(mapDica) ?? [];
 }
